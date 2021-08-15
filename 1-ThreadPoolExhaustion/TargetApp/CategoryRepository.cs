8e2ab0022dda85cc77d9c1ff5a914a2061946f9f
Praveen Raghuvanshi <praveenraghuvanshi@gmail.com>
2021-08-15 23:55:22 +0530

    Updated code
    Updated code

--- 1-ThreadPoolExhaustion/TargetApp/CategoryRepository.cs
+++ 1-ThreadPoolExhaustion/TargetApp/CategoryRepository.cs
@@ -16,31 +16,39 @@ namespace TargetApp
 
         private readonly ILogger<CategoryRepository> _logger;
         private readonly ProductDatabaseSettings _dbOptions;
+        private readonly IEnumerable<ProductCategory> _allCatergories;
 
         public CategoryRepository(IOptions<ProductDatabaseSettings> dbOptions, ILogger<CategoryRepository> logger)
         {
             _dbOptions = dbOptions?.Value ?? throw new ArgumentNullException(nameof(dbOptions)); ;
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            _allCatergories = new List<ProductCategory>
+            {
+                new ProductCategory(1, "Sony", 1),
+                new ProductCategory(2, "Apple", 1),
+                new ProductCategory(3, "Samsung", 2)
+            };
         }
 
         public IEnumerable<ProductCategory> GetAllCategories()
         {
-            using var db = DbConnection;
+            // using var db = DbConnection;
 
             // Because the DB query is quick, add a Task.Delay to simulate other slower tasks
-            Task.Delay(10).Wait();
+            Task.Delay(100).Wait();
 
-            return db.Query<ProductCategory>(@"SELECT ProductCategoryID Id, Name, ParentProductCategoryID ParentId FROM SalesLT.ProductCategory");
+            return _allCatergories; // db.Query<ProductCategory>(@"SELECT ProductCategoryID Id, Name, ParentProductCategoryID ParentId FROM SalesLT.ProductCategory");
         }
 
         public async Task<IEnumerable<ProductCategory>> GetAllCategoriesAsync()
         {
-            using var db = DbConnection;
+            // using var db = DbConnection;
 
             // Because the DB query is quick, add a Task.Delay to simulate other slower tasks
-            await Task.Delay(10);
+            await Task.Delay(100);
 
-            return await db.QueryAsync<ProductCategory>("SELECT ProductCategoryID Id, Name, ParentProductCategoryID ParentId FROM SalesLT.ProductCategory");
+            return await Task.FromResult<IEnumerable<ProductCategory>>(_allCatergories); // db.QueryAsync<ProductCategory>("SELECT ProductCategoryID Id, Name, ParentProductCategoryID ParentId FROM SalesLT.ProductCategory");
         }
 
         public ProductCategory GetCategory(int id)
